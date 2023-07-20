242d48d6301e68b50dbb6e8305dbb1417205e295
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-07-20 20:44:50 +0330

    Updates

--- server/server.sh
+++ server/server.sh
@@ -33,7 +33,9 @@ chmod +x install.sh
 
 ./install.sh
 
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm install v18
 
