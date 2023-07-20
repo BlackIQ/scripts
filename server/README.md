a10c27c2d7516ad0317eb5e63436f270ced28a85
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-07-20 20:15:12 +0330

    Add server

---
+++ server/README.md
@@ -0,0 +1,41 @@
+# Server
+
+Just server stuff.
+
+## About script
+
+> Details will add
+
+## Code
+
+> Details will all
+
+## How to use?
+
+You can use this script by following these steps:
+
+1. Download the script.
+
+```shell
+wget "https://raw.githubusercontent.com/BlackIQ/scripts/main/server/server.sh"
+```
+
+2. Give permission to script.
+
+```shell
+chmod +x server.sh
+```
+
+3. Run script with parameters.
+
+> Remember to run script as **root**
+
+```shell
+./server.sh
+```
+
+## Example
+
+```shell
+./server.sh
+```
---
+++ server/server.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Check that user is admin
+if [ $(whoami) != "root" ]; then
+  echo "This script must be run as root"
+  exit 1
+fi
+
+apt update && apt upgrade -y
+
+apt install nginx tmux neofetch net-tools nmap mysql-server gnupg -y
+
+type -p curl >/dev/null || (apt update && apt install curl  7 -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& apt update \
+&& apt install gh -y
+
+curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
+   gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg \
+   --dearmor
+
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+
+apt-get update
+
+apt-get install -y mongodb-org
+
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+source ~/.bashrc
+
+nvm install v18
+
+npm i -g pm2 serve create-next-app create-next-app nodemon
+
+systemctl start mysql
+systemctl start nginx
+systemctl start mongod
